34b215ec49a39a7503acc877b2298d8ed5712aa3
Karl Hepler <karl.hepler@gmail.com>
2014-12-13 13:03:08 -0500

    Initial Commit

---
+++ cloudmsg_backlink.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Cloud MSG Backlink Widget
+ * Plugin URI: http://www.cloudmsg.com
+ * Description: This plugin is a widget that shows a backlink to cloudmsg.com
+ * Version: 1.0
+ * Author: Karl Hepler
+ * Author URI: http://www.karlhepler.com
+ * Text Domain: cloudmsg
+ * Network: true
+ * License: GPL2
+ */
+
+/*  Copyright 2014  Karl Hepler  (email : karl.hepler@gmail.com)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License, version 2, as 
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
