0f2f12586bd4202c23c58fbaee2921217907052d
Karl Hepler <karl.hepler@gmail.com>
2014-12-15 20:03:54 -0500

    Changed from widget to footer and added version tag

--- cloudmsg_backlink.php
+++ cloudmsg_backlink.php
@@ -28,67 +28,13 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-class cloudmsg_backlink extends WP_Widget {
+function powered_by_cloudmsg() {
+  // enqueue the style
+  wp_enqueue_style( 'cloudmsg_backlink-style', plugins_url('style.css', __FILE__) );
 
-  // constructor
-  function __construct() {
-    $widget_ops = array('classname' => 'cloudmsg-backlink-widget', 'description' => __('This plugin is a widget that shows a backlink to cloudmsg.com', 'cloudmsg_backlink'));
-    parent::WP_Widget(false, $name = __('Cloud MSG Backlink Widget', 'cloudmsg_backlink'), $widget_ops );
-  }
-
-  // widget form creation
-  function form($instance) {  
-    if ($instance) {
-      $select = esc_attr($instance['select']);
-    }
-    else {
-      $select = '';
-    }
-    ?>
-    <p>
-      <label for="<?php echo $this->get_field_id('select'); ?>"><?php _e('Select', 'cloudmsg_backlink'); ?></label>
-      <select name="<?php echo $this->get_field_name('select'); ?>" id="<?php echo $this->get_field_id('select'); ?>" class="widefat">
-        <?php
-        $options = array('Image', 'Text');
-        foreach ($options as $option) {
-          echo '<option value="' . $option . '" id="' . $option . '"', $select == $option ? ' selected="selected"' : '', '>', $option, '</option>';
-        }
-        ?>
-      </select>
-    </p>
-    <?php
-  }
-
-  // widget update
-  function update($new_instance, $old_instance) {
-    $instance = $old_instance;
-
-    $instance['select'] = strip_tags($new_instance['select']);
-    return $instance;
-  }
-
-  // widget display
-  function widget($args, $instance) {
-    // enqueue the style
-    wp_enqueue_style( 'cloudmsg_backlink-style', plugins_url('style.css', __FILE__) );
-
-    extract($args);
-    echo $before_widget.'<div>';
-
-    switch ($instance['select']) {
-      case 'Image':
-        ?><a href="http://www.cloudmsg.com" target="_blank"><img src="<?php echo plugins_url('logo.png', __FILE__); ?>" alt="Powered By Cloud Managed Services Group, Inc."></a><?php
-        break;
-
-      case 'Text':
-      default:
-        ?><p>Powered by <a href="http://www.cloudmsg.com" target="_blank">Cloud Managed Services Group, Inc.</a></p><?php
-        break;
-    }
-
-    echo '</div>'.$after_widget;
-  }
+  ?>
+  <div class="cloudmsg-backlink"><p>Powered By <a href="http://www.cloudmsg.com" target="_blank">Cloud Managed Services Group, Inc.</a></p></div>
+  <?php
 }
-
-// register widget
-add_action('widgets_init', create_function('', 'return register_widget("cloudmsg_backlink");'));
+add_action('wp_footer', 'powered_by_cloudmsg');
+?>
